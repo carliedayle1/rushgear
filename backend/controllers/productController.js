bf8dd54739b9709cb57ff07eddbe30af1601a54c
Erick Dayle Loon <daylecarlie@gmail.com>
2020-11-22 13:43:17 +0800

    Prepare

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,7 +8,7 @@ import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler( async (req, res) => {
 
-    const pageSize = 3 
+    const pageSize = 10 
     const page = Number(req.query.pageNumber) || 1
 
     const keyword = req.query.keyword ? {
